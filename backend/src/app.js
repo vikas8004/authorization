ebe142be5a8b2ac7bd31c97f1a5ffb0d33ee2bc2
vikas <vikas80046@gmail.com>
2024-03-07 18:53:18 +0530

    made some changes

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,8 +5,13 @@ import cors from "cors";
 const app = express();
 app.use(express.json());
 app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(
   cors({
-    origin: "*",
+    origin: ["http://localhost:5173", "https://auth-frontend-teal.vercel.app"],
     credentials: true,
   })
 );
